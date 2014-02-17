fde926e156eab62fd49b33e08554a30f1b37cd2c
Agrass <Agrass_3840@gmail.com>
2014-02-17 16:25:03 +0400

    Changed Layout and Solve Percent of sum mass v.2

--- src/com/agrass/Calculator/DisplayAdditionActivity.java
+++ src/com/agrass/Calculator/DisplayAdditionActivity.java
@@ -4,13 +4,14 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.*;
 
 public class DisplayAdditionActivity extends Activity {
 
     String[] data_oils = {"Оливковое", "Растительное", "Облепиховое", "Эвкалиптовое", "Льняное", "Тыквенное", "Кокосовое"};
+    Spinner spinner;
+    ArrayAdapter<String> adapter;
 
     public void onCreate(Bundle savedInstanceState) {
 
@@ -22,10 +23,12 @@ public class DisplayAdditionActivity extends Activity {
                 getActionBar().setDisplayHomeAsUpEnabled(true);
             }
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, data_oils);
+        Intent intent = new Intent(this, MainActivity.class);
+
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, data_oils);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
-        Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        spinner = (Spinner) findViewById(R.id.spinner);
         spinner.setAdapter(adapter);
 
         spinner.setPrompt("Масла");//Title
@@ -36,43 +39,40 @@ public class DisplayAdditionActivity extends Activity {
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 // View element position
                 Toast.makeText(getBaseContext(), "Position = " + position, Toast.LENGTH_SHORT).show();
+
             }
             @Override
             public void onNothingSelected(AdapterView<?> arg0) {
             }
         });
+
     }
-
-
-
-
-
 
         public void buttonAdd(View v) {
 
-            Spinner spinner = (Spinner) findViewById(R.id.spinner);
+            spinner = (Spinner) findViewById(R.id.spinner);
 
             Intent intent = new Intent(this, MainActivity.class);
 
             EditText editTextMass = (EditText) findViewById(R.id.editText_Multiplier_First);
-            //EditText editTextMultiplierSecond = (EditText) findViewById(R.id.editText_Multiplier_Second);
 
-            String StringMass = editTextMass.getText().toString();
-            //String StringMultiplierSecond = editTextMultiplierSecond.getText().toString();
+            if (editTextMass.getText().toString().isEmpty() == true) {
 
-            String valueOfSpinner = spinner.getSelectedItem().toString();
+                Toast.makeText(getBaseContext(), "Укажите массу!", Toast.LENGTH_LONG).show();
 
-            //float multiplier_first = Float.valueOf(editTextMultiplierFirst.getText().toString());
+            } else {
 
+                String StringMass = editTextMass.getText().toString();
 
-            //float[] floatOil = {multiplier_first, multiplier_second};
+                String valueOfSpinner = spinner.getSelectedItem().toString();
 
-            String ans = valueOfSpinner+" - "+StringMass;
+                String ans = valueOfSpinner+" - "+StringMass+" - ";
 
-            intent.putExtra("floatOil", ans);
-            //intent.putExtra("ans",ans);
-            setResult(RESULT_OK, intent);
-            finish();
+                intent.putExtra("floatOil", ans);
+
+                setResult(RESULT_OK, intent);
+                finish();
+            }
         }
 
 }
--- src/com/agrass/Calculator/MainActivity.java
+++ src/com/agrass/Calculator/MainActivity.java
@@ -38,10 +38,6 @@ public class MainActivity extends Activity {
             TextView textview_answer = (TextView) findViewById(R.id.textView);
 
             float sum = 0;
-//            float[] percent = new float[2];
-//            int length = listView.getCount();
-//            float[] per = new float[length];
-//            String[] stringPer = new String[length];
 
             for (int i = 0; i < listView.getCount(); i++) {
 
@@ -49,20 +45,6 @@ public class MainActivity extends Activity {
                 Float item_float2 = Float.valueOf(item[1]);
 
                 sum += item_float2;
-
-            }
-
-            for (int i = 0; i < listView.getCount(); i++) {
-
-                String[] item = listView.getAdapter().getItem(i).toString().split(" - ");
-                Float item_float2 = Float.valueOf(item[1]);
-
-                Float item_float1 = (item_float2/sum)*100;
-
-                test.set(i,listView.getAdapter().getItem(i).toString()+" - "+Float.toString(item_float1)+"%");
-                adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, test);
-
-                listView.setAdapter(adapter);
 
             }
 
@@ -80,15 +62,43 @@ public class MainActivity extends Activity {
         @Override
         protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 
-            //TextView textView = (TextView) findViewById(R.id.textView1);
-
             if (data == null) { return; }
-
-            //ListItem th = new ListItem(data.getFloatArrayExtra("floatOil"));
 
             ArrayAdapter<String> adapterEdit = (ArrayAdapter<String>) listView.getAdapter();
             adapterEdit.add(data.getStringExtra("floatOil"));
             adapterEdit.notifyDataSetChanged();
+
+            TextView textview_answer = (TextView) findViewById(R.id.textView);
+
+            float sum = 0;
+
+            for (int i = 0; i < listView.getCount(); i++) {
+
+                String[] item = listView.getAdapter().getItem(i).toString().split(" - ");
+                Float item_float2 = Float.valueOf(item[1]);
+
+                sum += item_float2;
+
+            }
+
+            for (int i = 0; i < listView.getCount(); i++) {
+
+                String[] item = listView.getAdapter().getItem(i).toString().split(" - ");
+
+                String StringOil = item[0];
+
+                Float itemMass = Float.valueOf(item[1]);
+
+                int item_percent = (Math.round((itemMass/sum)*100)*100);
+
+                test.set(i,StringOil+" - "+item[1]+" - "+Float.toString(item_percent/100)+"%");
+                adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, test);
+
+                listView.setAdapter(adapter);
+
+
+            }
+
         }
 
 }
